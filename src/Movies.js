850e702b2df602a45f960dafd413debf5e242c77
Haoran Zhou <kens0serious@gmail.com>
2022-02-13 00:13:38 -0500

    completed search form

--- src/Movies.js
+++ src/Movies.js
@@ -12,7 +12,24 @@ const Movies = () => {
   if (loading) {
     return <div className="loading"></div>
   }
-  return <h2>movies component</h2>
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie
+        return (
+          <Link to={`/movies/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster === 'N/A' ? url : poster} alt={title} />
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        )
+      })}
+    </section>
+  )
 }
 
 export default Movies
