97c51e12acb7e00222b8dd67e36558cd218ae879
Haoran Zhou <kens0serious@gmail.com>
2022-02-12 23:42:06 -0500

    context state setup and initial fetch

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
-import Movies from './Movies'
 import Movie from './SingleMovie'
 
 function App() {
   return (
     <Routes>
       <Route path="/" exact element={<Home />} />
-      <Route path="/movies/:id" element={<Movie />}></Route>
+      <Route path="/movies/:id" element={<Movie />} />
     </Routes>
   )
 }
--- src/context.js
+++ src/context.js
@@ -4,7 +4,37 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState({ show: false, msg: '' })
+  const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState('batman')
+
+  const fetchMovies = async (url) => {
+    setLoading(true)
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      if (data.Response === 'True') {
+        setMovies(data.Search)
+        setError({ show: false, msg: '' })
+      } else {
+        setError({ show: true, msg: data.Error })
+      }
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`)
+  }, [query])
+
+  return (
+    <AppContext.Provider value={(loading, error, movies, query, setQuery)}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
